bd771e04c8ad796735ac8c9435cfafbc22700f78
Elvis M <u201112959@upc.edu.pe>
2024-06-19 12:45:54 -0500

    implemento los dataset data.json y se listo las citas en el componente principal App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { FaArchive } from "react-icons/fa";
 import "./App.css";
+import AddAppointment from "./components/AddAppointment";
 import Search from "./components/Search";
+import appointmentList from "./data.json";
+import { BiTrash } from "react-icons/bi";
 
 function App() {
   return (
@@ -10,6 +13,35 @@ function App() {
           <FaArchive className="inline-block text-red-400 align-top" />
           Your Appointments
         </h1>
+        <AddAppointment />
+        <Search />
+        <ul className="divide-y divide-gray-200">
+          {appointmentList.map((appointment) => (
+            <li key={appointment.id} className="px-3 py-3 flex items-start">
+              <button
+                type="button"
+                className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              >
+                <BiTrash />
+              </button>
+              <div className="flex-grow">
+                <div className="flex items-center">
+                  <span className="flex-none font-medium text-2xl text-blue-500">
+                    {appointment.petName}
+                  </span>
+                  <span className="flex-grow text-right">
+                    {appointment.aptDate}
+                  </span>
+                </div>
+                <div>
+                  <b className="font-bold text-blue-500">Owner:</b>{" "}
+                  {appointment.ownerName}
+                </div>
+                <div className="leading-tight">{appointment.aptNotes}</div>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
