0176aa15db8dfd33bd266af43aa0fa6847849274
Elvis M <u201112959@upc.edu.pe>
2024-06-19 22:30:31 -0500

    implementando busqueda por nombre de los siguientes campos de datos petName, ownerName y aptNotes

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -50,7 +50,7 @@ const DropDown = ({ toggle }) => {
   );
 };
 
-const Search = () => {
+const Search = ({ query, onQueryChange }) => {
   const [toggleSort, setToggleSort] = useState(false);
 
   return (
@@ -65,7 +65,8 @@ const Search = () => {
             type="text"
             name="query"
             id="query"
-            value=""
+            value={query}
+            onChange={(event) => onQueryChange(event.target.value)}
             className="pl-8 rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300"
             placeholder="Search"
           />
